4a4ccfb55c146c5e893fbc9519394daf8bc0f728
Vignesh <vigneshpamu2002@gmail.com>
2024-09-23 00:48:06 +0530

    safds

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
       </div>
-      <h1>Vite + CI/CD</h1>
+      <h1>Vite + CI/CD Pipeline implementated</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
